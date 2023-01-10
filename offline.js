2e00a2d64e8f7d4775d3e9e020a1a47a862ae130
Jimoh Quadri Olayemi <Jimohgadgetjar@gmail.com>
2023-01-10 19:45:04 +0100

    all upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1672221542,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/player-sheet0.png",
+		"images/box-sheet0.png",
+		"images/boxparticles.png",
+		"images/redcar-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/exit-sheet0.png",
+		"images/finish-sheet0.png",
+		"images/reward-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
